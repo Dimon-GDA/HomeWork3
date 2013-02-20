c4a9d52b684204d4e8bdea9bd2e7ed7948d14ae0
Dimon-GDA <dimon.gda@gmail.com>
2013-02-21 01:22:42 +0400

    Initial Commit

---
+++ HomeWork3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HomeWork3
+//
+//  Created by admin on 2/21/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
